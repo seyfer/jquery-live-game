9556554b6da1c5347192fa83968219b0641b572e
seyfer <seyferseed@mail.ru>
2013-11-03 18:33:37 +0800

    Game works!

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var dimension = 15;
+var dimension = 35;
 var chanceOfLiveCell = 0.5;
 var table;
 var cells;
@@ -11,6 +11,7 @@ $(document).ready(function() {
 
     cells = table.find('td');
     placeRandomCells();
+    playGame();
 
 });
 
@@ -25,7 +26,8 @@ function initializeGame()
 
         trHtml.push('<tr>');
         for (var x = 0; x < dimension; x++) {
-            trHtml.push('<td>&nbsp</td>');
+//            trHtml.push('<td>&nbsp</td>');
+            trHtml.push('<td></td>');
         }
 
         trHtml.push('</tr>');
@@ -34,7 +36,7 @@ function initializeGame()
 
     table.append($(trHtml));
 
-//    console.log(trHtml);
+//    consoleLog(trHtml);
 }
 
 /**
@@ -43,9 +45,11 @@ function initializeGame()
  */
 function placeRandomCells()
 {
+    consoleLog("placeRandomCells");
     for (var y = 0; y < dimension; y++) {
         for (var x = 0; x < dimension; x++) {
 
+            consoleLog("placeRandomCells");
             var cell = getCell(x, y);
             if (Math.random() > chanceOfLiveCell) {
                 cell.addClass('alive');
@@ -53,5 +57,159 @@ function placeRandomCells()
                 cell.removeClass('alive');
             }
         }
+    }
+}
+
+/**
+ * игра
+ * @returns {undefined}
+ */
+function playGame()
+{
+    playGeneration();
+    setTimeout('playGame()', 500);
+}
+
+/**
+ * процесс генерации
+ * @returns {undefined}
+ */
+function playGeneration()
+{
+    prepareNextGeneration();
+
+    consoleLog(cells);
+    renderNextGeneration();
+}
+
+/**
+ * подготовить генерацию
+ * @returns {undefined}
+ */
+function prepareNextGeneration()
+{
+    for (var y = 0; y < dimension; y++) {
+        for (var x = 0; x < dimension; x++) {
+
+            consoleLog("prepareNextGeneration");
+            var cell = getCell(x, y);
+            var neighbours = getLiveNeighbourCount(x, y);
+
+            cell.prop('isalive', 'false');
+            if (isCellAlive(x, y)) {
+
+                consoleLog(neighbours);
+
+                if (neighbours === 2 || neighbours === 3) {
+                    cell.prop('isalive', 'true');
+                }
+            } else if (neighbours === 3) {
+                cell.prop('isalive', 'true');
+            }
+        }
+    }
+}
+
+/**
+ * отрисовка
+ * @returns {undefined}
+ */
+function renderNextGeneration()
+{
+    cells.each(function() {
+
+        var cell = $(this);
+
+        cell.removeClass('alive');
+
+        if (cell.prop('isalive') === 'true') {
+            consoleLog(3);
+            cell.addClass('alive');
+        }
+
+        cell.prop('isalive', "false");
+    });
+}
+
+/**
+ * кол-во живых соседей
+ * @param {type} x
+ * @param {type} y
+ * @returns {Number}
+ */
+function getLiveNeighbourCount(x, y)
+{
+//    consoleLog("getLiveNeighbourCount");
+
+    var count = 0;
+    if (isCellAlive(x - 1, y - 1)) count++;
+    if (isCellAlive(x, y - 1)) count++;
+    if (isCellAlive(x + 1, y - 1)) count++;
+
+    if (isCellAlive(x - 1, y)) count++;
+    if (isCellAlive(x + 1, y)) count++;
+
+    if (isCellAlive(x - 1, y + 1)) count++;
+    if (isCellAlive(x, y + 1)) count++;
+    if (isCellAlive(x + 1, y + 1)) count++;
+
+    return count;
+}
+
+/**
+ * живая ли клетка
+ * @param {type} x
+ * @param {type} y
+ * @returns {unresolved}
+ */
+function isCellAlive(x, y)
+{
+    var cell = getCell(x, y);
+
+//    consoleLog(cell);
+//    consoleLog(cell.prop('class'));
+
+    return cell.prop('class') === 'alive';
+}
+
+/**
+ * получить ячейку
+ * @param {type} x
+ * @param {type} y
+ * @returns {unresolved}
+ */
+function getCell(x, y)
+{
+    if (x >= dimension) {
+        x = 0;
+    }
+    if (y >= dimension) {
+        y = 0;
+    }
+    if (x < 0) {
+        x = dimension - 1;
+    }
+    if (y < 0) {
+        y = dimension - 1;
+    }
+
+    var getted = $(cells[y * dimension + x]);
+//    consoleLog("getted");
+//    consoleLog(getted);
+
+    return getted;
+}
+
+/**
+ * лог переключалка
+ * @param {type} item
+ * @returns {undefined}
+ */
+function consoleLog(item)
+{
+    var log = false;
+
+    if (log) {
+        console.log(item);
     }
 }
